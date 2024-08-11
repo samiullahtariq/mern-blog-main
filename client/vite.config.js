a038be7e5d68874be7bbca73fe995db6c4e579e1
Samiullah Tariq Butt <91881023+samiullahtariq@users.noreply.github.com>
2024-08-11 11:49:54 +0500

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600
+  },
   server: {
     proxy: {
       '/api': {
