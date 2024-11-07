55b4114ab10bfbac20afd4b02e79cea138d96eb2
Samiullah Tariq Butt <91881023+samiullahtariq@users.noreply.github.com>
2024-11-07 16:42:38 +0500

    updated tailwind.config.js file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,17 @@
-export default{content:["./index.html","./src/**/*.{js,ts,jsx,tsx}","node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],theme:{extend:{}},plugins:[require("flowbite/plugin"),require("tailwind-scrollbar")]};
+import flowbitePlugin from "flowbite/plugin";
+import scrollbarPlugin from "tailwind-scrollbar";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    flowbitePlugin,
+    scrollbarPlugin
+  ]
+};
